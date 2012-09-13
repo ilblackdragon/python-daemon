72fde3abb3a814a2268accf4da94b61c39e53358
Tony Wang <wwwjfy@gmail.com>
2012-09-13 18:29:30 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='python-daemon',
+    version='0.0.1',
+    url='',
+    license='MIT',
+    author='Tony Wang',
+    author_email='wwwjfy@gmail.com',
+    description='forked from https://github.com/serverdensity/python-daemon'
+)
